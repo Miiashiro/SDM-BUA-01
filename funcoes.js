bbe09c7ac2f1df271fc4ff0759fa592f89e30a15
Miiashiro <miiashiro.caio1@gmail.com>
2025-03-18 11:03:38 -0300

    Trabalhando com funções

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+//1° forma
+function Nome(nome){
+    return `Olá, ${nome}`
+}
+
+console.log(Nome("Robert"))
+
+//2° Forma
+const soma = function(a, b){
+    return a + b
+}
+
+console.log(soma(10, 80))
+
+//3° forma
+const multi = (v, x) => v * x
+
+console.log(10, 10)
