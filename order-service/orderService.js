35e67d3011f6075d86e0afda9d9cd0032c9f734c
Miiashiro <miiashiro.caio1@gmail.com>
2025-04-22 11:05:05 -0300

    feat: requisicoes HTTP

--- order-service/orderService.js
+++ order-service/orderService.js
@@ -4,12 +4,62 @@ const app = Express()
 
 app.use(Express.json())
 
-app.post('/pedidos', (req, res) => {
-    const pedido = req.body
+let orders = []
 
-    console.log(pedido)
+app.post('/', async(req, res) => {
+    const {id, product, quantity} = req.body
 
-    res.send({message: `Pedido recebido`})
+    if(!id || !product || !quantity) {
+        return res.status(400).send({ message: "Dados do pedido inválidos"})
+    }
+
+    const newOrders = { id, product, quantity }
+    orders.push(newOrders)
+    res.status(201).send({message: "Pedido cadastrado com sucesso", orders: newOrders })
+
+    //orders.push(id, product, quantity)
+})
+
+app.get('/', async(req, res) => {
+    res.status(200).json(orders)
+})
+
+app.get('/pedidos/:id', async(req, res) => {
+    const orderId = req.params.id
+    const filteredProduct = orders.find(order => order.id === orderId)
+
+    if(!filteredProduct) return res.status(404).send({ message: "Produto não encontrado!" })
+
+    res.status(200).json(filteredProduct)
+})
+
+app.put('/pedidos/:id', async(req, res) => {
+    const orderId = req.params.id
+    const orderIndex = orders.findIndex(o => o.id === orderId)
+    
+    if(orderId === -1){
+        return res.status(404).send({ message: "Pedido não encontrado"})
+    }
+
+    const {product, quantity} = req.body
+
+    if(!product || !quantity){
+        return res.status(400).send({ message: "Dados inválidos!"})
+    }
+    orders[orderIndex] = {id: orderId, product, quantity}
+    res.status(200).send({ message: "Pedido atualizado com sucesso!", order: orders[orderIndex]})
+})
+
+app.delete('/pedidos/:id', async(req, res) => {
+    const orderId = req.params.id
+    const orderIndex = orders.findIndex(o => o.id === orderId)
+
+    if(orderId === -1){
+        return res.status(404).send({ message: "Pedido não encontrado"})
+    }
+
+    orders.splice(orderIndex, 1)
+    res.status(200).send({ message: "pedido excluido com sucesso!"})
 })
 
 app.listen(4000, () => console.log("rodando na porta 4000"))
--- user-service/userService.js
+++ user-service/userService.js
@@ -6,12 +6,12 @@ const app = Express()
 
 app.use(Express.json())
 
-app.post('/usuarios', async(req, res) => {
-    const usuario = req.body
+app.post('/', async(req, res) => {
+    /*const usuario = req.body
 
     await axios.post('http://localhost:4000/pedidos', {userId: usuario.id});
 
-    res.send({message: `Usuario cadastrado com sucesso`, usuario})
+    res.send({message: `Usuario cadastrado com sucesso`, usuario})*/
 })
 
 app.listen(3000, () => console.log("Funcionando na porta 3000"));
